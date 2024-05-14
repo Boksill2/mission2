4c00f5c3a065a3caa0824077e8623b28c2bff39e
dlsdusk <dlsdusk@naver.com>
2024-05-14 11:21:15 +0900

    1-100더하기

--- mission/src/main/java/mission/Mission.java
+++ mission/src/main/java/mission/Mission.java
@@ -3,8 +3,15 @@ package mission;
 public class Mission {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		int result=0;
+		
+		for(int i =1; i<=100;i++) {
+			result += i;
+		}
+			System.out.println(result);
+		
 	}
 
 }
