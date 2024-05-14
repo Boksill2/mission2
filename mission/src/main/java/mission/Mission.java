2539dcc0f03a971cd9db2c947e5702d67bfa00a0
dlsdusk <dlsdusk@naver.com>
2024-05-14 11:23:07 +0900

    1-10까지 출력

--- mission/src/main/java/mission/Mission.java
+++ mission/src/main/java/mission/Mission.java
@@ -5,12 +5,12 @@ public class Mission {
 	public static void main(String[] args) {
 		
 		
-		int result=0;
 		
-		for(int i =1; i<=100;i++) {
-			result += i;
+		
+		for(int i =1; i<=10;i++) {
+			System.out.println(i);
 		}
-			System.out.println(result);
+			
 		
 	}
 
