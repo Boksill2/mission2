441bcb0f2d968db55fb70013d4b34701210060fa
dlsdusk <dlsdusk@naver.com>
2024-05-14 11:23:48 +0900

    1-100더하기

---
+++ mission/src/main/java/mission/Mission2.java
@@ -0,0 +1,15 @@
+package mission;
+
+public class Mission2 {
+
+	public static void main(String[] args) {
+		
+			int result=0;
+		
+		for(int i =1; i<=100;i++) {
+			result += i;
+		}
+			System.out.println(result);
+	}
+
+}
